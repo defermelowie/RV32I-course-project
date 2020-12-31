92a69befa5ee2a38fe79bbab35586d10b708d689
lowie <lowiecode@gmail.com>
2020-12-31 12:38:33 +0100

    Changed comments

--- src/forwarding_codes.h
+++ src/forwarding_codes.h
@@ -1,5 +1,5 @@
 parameter FORWARDING_CODE_SIZE = 2;
 // -- Forwarding encoding -------------------------------------
-parameter REG_FILE = 'b00;      // Alu operand must come from register file or immediate generator (based on control:alu_src)
+parameter REG_FILE = 'b00;      // Alu operand must come from register file
 parameter ALU_RESULT = 'b01;    // Alu operand must come from prior alu result
 parameter MEMORY = 'b10;        // Alu operand must come from memory or from prior alu result (based on control:mem_to_reg) 
