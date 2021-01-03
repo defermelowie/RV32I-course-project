c09846e00c4a6c97781fd133ddde7400a4eac380
lowie <lowiecode@gmail.com>
2021-01-03 15:30:48 +0100

    Renamed file

--- src/forwarding_unit.v
+++ src/forwarding_unit.v
@@ -1,12 +1,12 @@
 module forwarding_unit (
+    ID_instruction,             // input -> instruction in the instruction decode stage
+    EX_reg_write_enable,        // input -> "reg_write_enable" from execution stage
+    EX_instruction,             // input -> instruction in the execution stage
     MEM_reg_write_enable,       // input -> "reg_write_enable" from memory access stage
-    WB_reg_write_enable,        // input -> "reg_write_enable" from write back stage
-    MEM_destination_register,   // input -> destination register from memory access stage
-    WB_destination_register,    // input -> destination register from write back stage
-    EX_read_register_0,         // input -> read register 0 from execution stage
-    EX_read_register_1,          // input -> read register 1 from execution stage
-    forward_0,                  // output -> forwarding code for alu operand 0 
-    forward_1                   // output -> forwarding code for alu operand 1 
+    MEM_instruction,            // input -> instruction in the memory access stage
+    MEM_mem_to_reg,             // input -> "mem_to_reg" form the memory access stage
+    forward_mode_0,             // output -> forwarding mode for data 0 
+    forward_mode_1              // output -> forwarding mode for data 1 
 );
 
 // -- Include definitions -------------------------------------
@@ -14,45 +14,72 @@ module forwarding_unit (
 `include "forwarding_codes.h"   // Contains forwarding code definitions   
 
 // -- Module IO -----------------------------------------------
-input MEM_reg_write_enable, WB_reg_write_enable;
-input [4:0] MEM_destination_register, WB_destination_register, EX_read_register_0, EX_read_register_1;
-output reg [FORWARDING_CODE_SIZE-1:0] forward_0, forward_1;
+input EX_reg_write_enable, MEM_reg_write_enable, MEM_mem_to_reg;
+input [31:0] ID_instruction, EX_instruction, MEM_instruction;
+output reg [FORWARDING_CODE_SIZE-1:0] forward_mode_0, forward_mode_1;
+
+// -- Internal signals ----------------------------------------
+wire [4:0] ID_destination_register = ID_instruction[11:7];
+wire [4:0] ID_read_register_0 = ID_instruction[19:15];
+wire [4:0] ID_read_register_1 = ID_instruction[24:20];
+wire [4:0] EX_destination_register = EX_instruction[11:7];
+wire [4:0] MEM_destination_register = MEM_instruction[11:7];
 
 // -- Determine hazard ----------------------------------------
-wire ex_hazard_0 = (
+wire ex_alu_0 = (
+    EX_reg_write_enable && 
+    (EX_destination_register != 0) &&
+    (EX_destination_register == ID_read_register_0)
+);
+
+wire ex_alu_1 = (
+    EX_reg_write_enable && 
+    (EX_destination_register != 0) &&
+    (EX_destination_register == ID_read_register_1)
+);
+
+wire mem_mem_0 = (
     MEM_reg_write_enable && 
     (MEM_destination_register != 0) &&
-    (MEM_destination_register == EX_read_register_0)
+    MEM_mem_to_reg &&
+    (MEM_destination_register == ID_read_register_0)
 );
-wire ex_hazard_1 = (
+
+wire mem_mem_1 = (
     MEM_reg_write_enable && 
     (MEM_destination_register != 0) &&
-    (MEM_destination_register == EX_read_register_1)
+    MEM_mem_to_reg &&
+    (MEM_destination_register == ID_read_register_1)
 );
-wire mem_hazard_0 = (
-    WB_reg_write_enable &&
-    (WB_destination_register != 0) &&
-    !ex_hazard_0 &&
-    (WB_destination_register == EX_read_register_0)
+
+wire mem_alu_0 = (
+    MEM_reg_write_enable && 
+    (MEM_destination_register != 0) &&
+    !MEM_mem_to_reg &&
+    (MEM_destination_register == ID_read_register_0)
 );
-wire mem_hazard_1 = (
-    WB_reg_write_enable &&
-    (WB_destination_register != 0) &&
-    !ex_hazard_1 &&
-    (WB_destination_register == EX_read_register_1)
+
+wire mem_alu_1 = (
+    MEM_reg_write_enable && 
+    (MEM_destination_register != 0) &&
+    !MEM_mem_to_reg &&
+    (MEM_destination_register == ID_read_register_1)
 );
 
 // -- Set forwarding signals ----------------------------------
 always @(*) begin
     case ('b1)
-        ex_hazard_0: forward_0 <= ALU_RESULT;
-        mem_hazard_0: forward_0 <= MEMORY;
-        default: forward_0 <= REG_FILE;
+        ex_alu_0: forward_mode_0 <= EX_ALU_OUT;
+        mem_mem_0: forward_mode_0 <= MEM_MEM_OUT;
+        mem_alu_0: forward_mode_0 <= MEM_ALU_OUT;
+        default: forward_mode_0 <= ID_REG_OUT;
     endcase
+
     case ('b1)
-        ex_hazard_1: forward_1 <= ALU_RESULT;
-        mem_hazard_1: forward_1 <= MEMORY;
-        default: forward_1 <= REG_FILE;
+        ex_alu_1: forward_mode_1 <= EX_ALU_OUT;
+        mem_mem_1: forward_mode_1 <= MEM_MEM_OUT;
+        mem_alu_1: forward_mode_1 <= MEM_ALU_OUT;
+        default: forward_mode_1 <= ID_REG_OUT;
     endcase    
 end
 
