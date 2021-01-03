f3ef35be7ac89978b3f04bb1d18fd33887f8ba34
lowie <lowiecode@gmail.com>
2021-01-03 15:26:46 +0100

    Removed old code

--- src/forwarding_codes.h
+++ src/forwarding_codes.h
@@ -1,11 +1,4 @@
 parameter FORWARDING_CODE_SIZE = 2;
-/*
-// -- Forwarding encoding -------------------------------------
-parameter REG_FILE = 'b00;      // Alu operand must come from register file
-parameter ALU_RESULT = 'b01;    // Alu operand must come from prior alu result
-parameter MEMORY = 'b10;        // Alu operand must come from memory or from prior alu result (based on control:mem_to_reg) 
-*/
-
 // -- Forwarding encoding -------------------------------------
 parameter ID_REG_OUT = 'b00;    // Data must come from register file
 parameter EX_ALU_OUT = 'b01;    // Data must come from alu result in EX stage
