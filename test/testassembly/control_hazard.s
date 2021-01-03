52a08d791cb8105e32700fa4f4716e4ecb88ead0
lowie <lowiecode@gmail.com>
2021-01-03 13:28:19 +0100

    Added control hazard test assembly

---
+++ test/testassembly/control_hazard.s
@@ -0,0 +1,41 @@
+# Normal operation: branch not taken
+li t0, 7
+li t1, 8
+nop
+nop
+nop
+beq t0, t1, branch_1
+li t3, 1
+branch_1:
+nop
+
+# Normal operation: branch taken
+li t0, 9
+li t1, 9
+nop
+nop
+nop
+beq t0, t1, branch_2
+li t3, 2
+branch_2:
+nop
+
+# Control hazard: EX stage
+li t0, 5
+nop
+li t1, 5    # Data in EX stage
+nop
+beq t0, t1, branch_3
+li t3, 3
+branch_3:
+nop
+
+# Control hazard: MEM stage
+li t0, 7
+li t1, 7    # Data in MEM stage
+nop
+nop
+beq t0, t1, branch_4
+li t3, 3
+branch_4:
+nop
