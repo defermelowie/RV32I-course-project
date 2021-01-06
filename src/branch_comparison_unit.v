b208cae9e61d03f5aaa262be7b8a5b3c6fc18fa0
lowie <lowiecode@gmail.com>
2021-01-06 12:07:22 +0100

    Fixed problems with signed comparisons

--- src/branch_comparison_unit.v
+++ src/branch_comparison_unit.v
@@ -15,18 +15,29 @@ input wire [2:0] mode;
 output reg branch;
 
 // -- Internal signals ----------------------------------------
-wire signed [XLEN-1:0] in_0_signed, in_1_signed;
-assign in_0_signed = in_0;
-assign in_1_signed = in_1;
 
 // -- process to decide to branch -----------------------------
 always @(*) begin
-    branch <= 0;    // Preset branch to zero, then set in neccesary
+    branch <= 0;    // Preset branch to zero, then set if neccesary
     case (mode)
         EQ: if (in_0 == in_1) branch <= 1;
         NE: if (in_0 != in_1) branch <= 1;
-        LT: if (in_0_signed < in_1_signed) branch <= 1;
-        GE: if (in_0_signed >= in_1_signed) branch <= 1;
+        LT: begin
+                if (!in_0[XLEN-1] && in_1[XLEN-1])          // in_0 is positive and in_1 is negative
+                    branch <= 0;
+                else if (in_0[XLEN-1] && !in_1[XLEN-1])     // in_0 is negative and in_1 is positive
+                    branch <= 1;
+                else                                        // in_0 and in_1 have equal sign
+                    branch <= (in_0 < in_1);
+            end
+        GE: begin
+                if (!in_0[XLEN-1] && in_1[XLEN-1])          // in_0 is positive and in_1 is negative
+                    branch <= 1;
+                else if (in_0[XLEN-1] && !in_1[XLEN-1])     // in_0 is negative and in_1 is positive
+                    branch <= 0;
+                else                                        // in_0 and in_1 have equal sign
+                    branch <= (in_0 >= in_1);
+            end
         LTU: if (in_0 < in_1) branch <= 1;
         GEU: if (in_0 >= in_1) branch <= 1;
         default: branch <= 0; // In case of unknown mode, do not branch
