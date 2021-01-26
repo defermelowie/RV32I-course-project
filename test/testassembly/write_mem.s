2bb6402e8968800f19cdc68685bee9479ade4434
lowie <lowiecode@gmail.com>
2021-01-26 18:31:02 +0100

    Write to IO

--- test/testassembly/write_mem.s
+++ test/testassembly/write_mem.s
@@ -2,9 +2,9 @@ li t0, 0x0
 li t1, 0x200
 li s2, 0x2000    # IO base address
 loop:
-    sw t0, 0(t0)
-    sw t0, 0(t2)
-    sw t0, 1(t2)
+    sb t0, 0(t0)
+    sw t0, 0(s2)
+    sw t0, 1(s2)
     jal time_consuming_loop
     addi t0, t0, 1
     bne t0, t1, loop
