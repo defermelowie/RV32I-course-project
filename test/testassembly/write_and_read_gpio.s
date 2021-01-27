5af422de034b1ea2dd736b83a3f460d7892cb9dc
lowie <lowiecode@gmail.com>
2021-01-27 17:12:30 +0100

    Added nightrider

--- test/testassembly/write_and_read_gpio.s
+++
@@ -1,9 +0,0 @@
-
-addi s0, zero, 0x400	 	#basisadres inputs
-addi t0, zero, 0x3FF 	#alle leds aan
-add s1, s0, s0			#basisadres outputs
-sw t0, 0(s1)
-lw a0, 0(s0)
-lw a1, 1(s0)
-lw x12, 2(s0)
-
--- test/testassembly/write_mem.s
+++ test/testassembly/write_mem.s
@@ -4,7 +4,7 @@ li s2, 0x2000    # IO base address
 loop:
     sb t0, 0(t0)
     sw t0, 0(s2)
-    sw t0, 1(s2)
+    sw t0, 4(s2)
     jal time_consuming_loop
     addi t0, t0, 1
     bne t0, t1, loop
